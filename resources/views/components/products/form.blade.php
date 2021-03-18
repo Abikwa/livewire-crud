affb6aece2efd0d50aebf7e2e52c5188d00319b7
Abikwa <Abikalex8@gmail.com>
2021-03-18 10:45:00 +0200

    update modal form

--- resources/views/components/products/form.blade.php
+++ resources/views/components/products/form.blade.php
@@ -1,16 +1,11 @@
 
-
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-	Open Form
-</button>
-
 <!-- Modal -->
 <div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">New product</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close"  wire:click.prevent="cancel()" data-dismiss="modal" aria-label="Close">
                      <span aria-hidden="true close-btn">×</span>
                 </button>
             </div>
@@ -23,13 +18,13 @@
                     </div>
                     <div class="form-group">
                         <label for="exampleFormControlInput2">Price</label>
-                        <input type="number" class="form-control" id="exampleFormControlInput2" wire:model="price product" placeholder="Enter price">
+                        <input type="number" class="form-control" id="exampleFormControlInput2" wire:model="price" placeholder="Enter price">
                         @error('price') <span class="text-danger error">{{ $message }}</span>@enderror
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary close-btn" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary close-btn" wire:click.prevent="cancel()" data-dismiss="modal">Close</button>
                 <button type="button" wire:click.prevent="store()" class="btn btn-primary close-modal">Save changes</button>
             </div>
         </div>
