23cc6fb114f7b6a0a32dbd5e4ff87b0b3f135584
Abikwa <Abikalex8@gmail.com>
2021-03-18 10:45:19 +0200

    update product livewire

--- resources/views/livewire/products/product.blade.php
+++ resources/views/livewire/products/product.blade.php
@@ -1,3 +1,15 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-</div>
+
+ 
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+     
+    <div class="text-info">
+        <h1>Products</h1>
+    </div>
+    <div class="text-right">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            <i class="fa fa-plus"></i>
+        </button>
+    </div>
+    <x-products.form />
+    <x-products.data />
+</main>
