f83ae58bbd811f7e2eab0dbad88555f42fe4286b
Abikwa <Abikalex8@gmail.com>
2021-03-18 08:47:21 +0200

    update component data

--- resources/views/components/products/data.blade.php
+++ resources/views/components/products/data.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-</div>
+
+ <table class="table table-bordered mt-5">
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>price</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($this->products as $value)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $value->name }}</td>
+            <td>{{ $value->price }}</td>
+            <td>
+            <button data-toggle="modal" data-target="#updateModal" wire:click="edit({{ $value->id }})" class="btn btn-primary btn-sm">Edit</button>
+            <button wire:click="delete({{ $value->id }})" class="btn btn-danger btn-sm">Delete</button>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
