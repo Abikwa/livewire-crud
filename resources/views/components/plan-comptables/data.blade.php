83c36aaf23021a1a2999f6dd3cbf4817f94c0760
Abikwa <Abikalex8@gmail.com>
2021-06-14 14:44:51 +0200

    add comptenetr

--- resources/views/components/plan-comptables/data.blade.php
+++ resources/views/components/plan-comptables/data.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+
+ <table class="table table-bordered mt-5">
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>Code compte</th>
+            <th>Libelle</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($this->plancomptables as $value)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $value->codecompte }}</td>
+            <td>{{ $value->libelle }}</td>
+            <td>
+            <button data-toggle="modal" data-target="#exampleModal" wire:click="edit({{ $value->id }})" class="btn btn-primary btn-sm">Edit</button>
+            <button wire:click="delete({{ $value->id }})" class="btn btn-danger btn-sm">Delete</button>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
